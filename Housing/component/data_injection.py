6c8654d7bc35dab8891540b992275a43dec55838
unknown <krishnavizster@gmail.com>
2022-06-25 01:52:03 -0700

    components updated

---
+++ Housing/component/data_injection.py
@@ -0,0 +1,15 @@
+
+
+
+
+from Housing.entity.config_entity import DataIngestionConfig
+import sys,os
+from housing.exception import HousingException 
+
+class DataIngestion:
+
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
+        try:
+            pass
+            
+        pass 
